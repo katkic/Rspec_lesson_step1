ccf8f0050d61c5426b5fa5e0e31f142737bbee65
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-11-27 17:37:28 +0900

    CRUD機能を実装

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2019_11_26_141941) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
-    t.text "discription"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
