e3a5f95bd8a6c771c0dcdf8aec88c6cac44131aa
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-12-02 20:55:45 +0900

    タスクの終了期限のバリデーションを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,13 @@
 class Task < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { maximum: 30 }
   validates :description, presence: true
+  validate :check_expired_at
+
+
+
+  private
+
+  def check_expired_at
+    errors.add(:expired_at, 'の日時を正しく入力してください') if expired_at <= Time.now
+  end
 end
