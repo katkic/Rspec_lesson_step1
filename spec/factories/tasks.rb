71e1041682422c992f360575accf11412f5a1ea9
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-12-02 23:23:33 +0900

    タスクの終了期限のsystem specを実装

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,13 @@
 FactoryBot.define do
   factory :task do
-    sequence(:name) { |n| "task_#{n}" }
-    sequence(:description) { |n| "Factoryで作ったtask_#{n}です" }
+    name { 'タスク1' }
+    description { 'Factoryで作ったタスク1です' }
+    expired_at { Time.new(2020, 1, 10, 17, 0) }
+  end
+
+  factory :second_task, class: Task do
+    name { 'タスク2' }
+    description { 'Factoryで作ったタスク2です' }
+    expired_at { Time.new(2019, 12, 10, 22, 30) }
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'タスク管理機能', type: :system do
   before do
-    2.times { create(:task) }
+    create(:task)
+    create(:second_task)
   end
 
   describe 'タスク一覧画面' do
@@ -10,7 +11,7 @@ RSpec.describe 'タスク管理機能', type: :system do
       it '作成済みのタスクが表示されること' do
         visit tasks_path
 
-        expect(page).to have_content 'task_1'
+        expect(page).to have_content 'タスク1'
       end
     end
 
@@ -19,8 +20,18 @@ RSpec.describe 'タスク管理機能', type: :system do
         visit tasks_path
         task_list = all('.task_row') # タスク一覧を配列として取得するため、View側でidを振っておく(要素にtask_worというクラスをつけておく)
 
-        expect(task_list[0]).to have_content 'task_4'
-        expect(task_list[1]).to have_content 'task_3'
+        expect(task_list[0]).to have_content 'タスク2'
+        expect(task_list[1]).to have_content 'タスク1'
+      end
+
+      it 'タスクが終了期限の降順で並んでいること' do
+        visit tasks_path
+        click_on '終了期限でソート'
+        sleep 1
+        task_list = all('.task_row')
+
+        expect(task_list[0]).to have_content 'タスク1'
+        expect(task_list[1]).to have_content 'タスク2'
       end
     end
   end
@@ -31,11 +42,17 @@ RSpec.describe 'タスク管理機能', type: :system do
         visit new_task_path
         fill_in '名称', with: 'task_test'
         fill_in '詳しい説明', with: 'タスクの登録テスト'
+        select '2019', from: 'task_expired_at_1i'
+        select '12', from: 'task_expired_at_2i'
+        select '31', from: 'task_expired_at_3i'
+        select '17', from: 'task_expired_at_4i'
+        select '00', from: 'task_expired_at_5i'
         click_on '登録する'
 
         expect(page).to have_content 'タスク「task_test」を登録しました'
         expect(page).to have_content 'task_test'
         expect(page).to have_content 'タスクの登録テスト'
+        expect(page).to have_content '2019年12月31日 17:00'
       end
     end
   end
@@ -43,11 +60,17 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe 'タスク詳細画面' do
     context '任意のタスク詳細画面に遷移した場合' do
       it '該当タスクの内容が表示されたページに遷移すること' do
-        task = create(:task, name: 'task_1', description: 'taskの登録テストです')
+        task = create(
+          :task,
+          name: 'タスク3',
+          description: 'タスクの登録テストです',
+          expired_at: Time.new(2019, 12, 10, 22, 30)
+        )
         visit task_path(task)
 
-        expect(page).to have_content 'task_1'
-        expect(page).to have_content 'taskの登録テストです'
+        expect(page).to have_content 'タスク3'
+        expect(page).to have_content 'タスクの登録テストです'
+        expect(page).to have_content '2019年12月10日 22:30'
       end
     end
   end
