8b7ff9c91ed295f42e51169e6c5c5ebd33824435
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-12-06 19:09:39 +0900

    タスク検索のモデルテストを実装

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,62 +1,122 @@
 require 'rails_helper'
 
 RSpec.describe 'タスク管理機能', type: :model do
-  it 'nameが空ならバリデーションが通らない' do
-    task = Task.new(
-      name: '',
-      description: '失敗テスト',
-      expired_at: Time.now + 60 * 60 * 24
-    )
-    expect(task).not_to be_valid
+  describe 'タスク新規登録時のバリデーション' do
+    it 'nameが空ならバリデーションが通らない' do
+      task = Task.new(
+        name: '',
+        description: '失敗テスト',
+        expired_at: Time.now + 60 * 60 * 24
+      )
+      expect(task).not_to be_valid
+    end
+
+    it 'descriptionが空なら、バリデーションが通らない' do
+      task = Task.new(
+        name: '失敗テスト',
+        description: '',
+        expired_at: Time.now + 60 * 60 * 24
+      )
+      expect(task).not_to be_valid
+    end
+
+    it 'nameとdescriptionに内容が記載されていれば、バリデーションが通る' do
+      task = Task.new(
+        name: '成功テスト',
+        description: 'テストが成功しました！',
+        expired_at: Time.now + 60 * 60 * 24
+      )
+      expect(task).to be_valid
+    end
+
+    it 'nameが30文字より大きければ、バリデーションが通らない' do
+      task = Task.new(
+        name: 'a' * 31,
+        description: '失敗テスト',
+        expired_at: Time.now + 60 * 60 * 24
+      )
+      expect(task).not_to be_valid
+    end
+
+    it 'nameが重複していたら、バリデーションが通らない' do
+      Task.create(
+        name: 'タスク1',
+        description: 'タスク1です',
+        expired_at: Time.now + 60 * 60 * 24
+      )
+      task = Task.new(
+        name: 'タスク1',
+        description: 'タスク名称の重複は認められません',
+        expired_at: Time.now + 60 * 60 * 24
+      )
+      expect(task).not_to be_valid
+    end
+
+    it 'expired_atが現在日時以前の場合、バリデーションが通らない' do
+      task = Task.create(
+        name: 'タスク1',
+        description: 'タスク1です',
+        expired_at: Time.now
+      )
+      expect(task).not_to be_valid
+    end
   end
 
-  it 'descriptionが空なら、バリデーションが通らない' do
-    task = Task.new(
-      name: '失敗テスト',
-      description: '',
-      expired_at: Time.now + 60 * 60 * 24
-    )
-    expect(task).not_to be_valid
-  end
+  describe 'タスク検索機能' do
+    before do
+      @task = Task.create(
+        name: 'ポラス新松戸10期',
+        description: '検索テスト用のタスクデータです',
+        expired_at: Time.now + 60 * 60 * 24,
+        status: 'in_progress'
+      )
 
-  it 'nameとdescriptionに内容が記載されていれば、バリデーションが通る' do
-    task = Task.new(
-      name: '成功テスト',
-      description: 'テストが成功しました！',
-      expired_at: Time.now + 60 * 60 * 24
-    )
-    expect(task).to be_valid
-  end
+      @other_task = Task.create(
+        name: 'ポラス流山7期',
+        description: '検索テスト用のタスクデータ2です',
+        expired_at: Time.now + 60 * 60 * 24,
+        status: 'not_started'
+      )
+    end
 
-  it 'nameが30文字より大きければ、バリデーションが通らない' do
-    task = Task.new(
-      name: 'a' * 31,
-      description: '失敗テスト',
-      expired_at: Time.now + 60 * 60 * 24
-    )
-    expect(task).not_to be_valid
-  end
+    context 'タスク名を「新松戸」、ステータスは指定なしで検索した場合' do
+      it 'タスク名が「新松戸」を含むタスクを返す' do
+        params = { name: '新松戸', status: '' }
+        expect(Task.search(params)).to include(@task)
+      end
 
-  it 'nameが重複していたら、バリデーションが通らない' do
-    Task.create(
-      name: 'タスク1',
-      description: 'タスク1です',
-      expired_at: Time.now + 60 * 60 * 24
-    )
-    task = Task.new(
-      name: 'タスク1',
-      description: 'タスク名称の重複は認められません',
-      expired_at: Time.now + 60 * 60 * 24
-    )
-    expect(task).not_to be_valid
-  end
+      it '他のタスクを返さない' do
+        params = { name: '新松戸', status: '' }
+        expect(Task.search(params)).to_not include(@other_task)
+      end
+    end
 
-  it 'expired_atが現在日時以前の場合、バリデーションが通らない' do
-    task = Task.create(
-      name: 'タスク1',
-      description: 'タスク1です',
-      expired_at: Time.now
-    )
-    expect(task).not_to be_valid
+    context 'タスク名を「テスト」、ステータスは指定なしで検索した場合' do
+      it '空を返す' do
+        params = { name: 'テスト', status: '' }
+        expect(Task.search(params)).to be_empty
+      end
+    end
+
+    context 'タスク名を「新松戸」、ステータスは指定なしで検索した場合' do
+      it 'タスク名が「新松戸」を含むタスクを返す' do
+        params = { name: '新松戸', status: '' }
+        expect(Task.search(params)).to include(@task)
+      end
+    end
+
+    context 'タスク名は指定なし、ステータスを「着手中」で検索した場合' do
+      it 'ステータスが「着手中」のタスクを返す' do
+        params = { name: '', status: 'in_progress' }
+        expect(Task.search(params)).to include(@task)
+      end
+    end
+
+    context 'タスク名を「タスク」、ステータスを「着手中」で検索した場合' do
+      it 'タスク名が「タスク」' do
+        params = { name: '新松戸', status: 'in_progress' }
+        expect(Task.search(params)).to include(@task)
+      end
+    end
   end
 end
