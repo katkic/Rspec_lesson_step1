ddd151f0a8fac17bc1004feb60a97a987bd6a712
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-12-06 20:05:42 +0900

    タスクのシードデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+(1..100).each do |i|
+  status = rand(3)
+  Task.create!(
+    name: "タスク#{i}",
+    description: "タスクの登録テスト#{i}です",
+    expired_at: Time.now + 60 * 60 * 24 * i,
+    status: status
+  )
+end
