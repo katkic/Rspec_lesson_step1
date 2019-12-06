fe6d67ad07f39e02aa14463905eb9333e7e76beb
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-12-06 19:08:45 +0900

    タスク検索に終了期限でソートする機能を追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -12,11 +12,12 @@ class Task < ApplicationRecord
   scope :search, -> (search_params) do
     return if search_params.blank?
 
-    name_like(search_params[:name]).status_is(search_params[:status])
+    name_like(search_params[:name]).status_is(search_params[:status]).expired_sort(search_params[:expired_at])
   end
 
   scope :name_like, -> (name) { where('name LIKE ?', "%#{name}%") if name.present? }
   scope :status_is, -> (status) { where(status: status) if status.present? }
+  scope :expired_sort, -> (expired_at) { order(expired_at: :desc) if expired_at == 'true' }
 
   private
 
