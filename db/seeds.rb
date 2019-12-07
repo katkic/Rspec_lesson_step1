4bcd88237c2ed0d205924ccd0f3ba86f6a4536c0
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-12-07 11:08:02 +0900

    タスクのシードデータを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 (1..100).each do |i|
-  status = rand(3)
   Task.create!(
     name: "タスク#{i}",
     description: "タスクの登録テスト#{i}です",
     expired_at: Time.now + 60 * 60 * 24 * i,
-    status: status
+    status: rand(3),
+    priority: rand(3)
   )
 end
