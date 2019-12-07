7dd4a1fea1b41a07120c9b4b465f12789e34b0e3
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-12-07 11:10:42 +0900

    タスク検索のモデルテストを修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,22 +1,28 @@
 FactoryBot.define do
-  factory :task do
-    name { 'タスク1' }
+  factory :task1, class: Task do
+    name { '植栽1' }
     description { 'Factoryで作ったタスク1です' }
-    expired_at { Time.new(2020, 1, 10, 17, 0) }
+    created_at { Time.new(2019, 12, 5, 10, 0) }
+    expired_at { Time.new(2019, 12, 14, 17, 0) }
+    status { 2 } # 完了
+    priority { 2 } # 高
   end
 
   factory :task2, class: Task do
-    name { 'タスク2' }
+    name { '植栽2' }
     description { 'Factoryで作ったタスク2です' }
-    created_at { Time.new(2019, 12, 7, 10, 0) }
-    expired_at { Time.new(2019, 12, 10, 22, 30) }
+    created_at { Time.new(2019, 12, 6, 10, 0) }
+    expired_at { Time.new(2019, 12, 24, 22, 30) }
+    status { 1 } # 着手中
+    priority { 1 } # 中
   end
 
   factory :task3, class: Task do
-    name { 'テスト1' }
+    name { '伐採1' }
     description { 'Factoryで作ったタスク3です' }
-    created_at { Time.new(2019, 12, 5, 10, 0) }
-    expired_at { Time.new(2019, 12, 7, 17, 0) }
-    status { 2 }
+    created_at { Time.new(2019, 12, 7, 10, 0) }
+    expired_at { Time.new(2019, 12, 31, 17, 0) }
+    status { 0 } # 未着手
+    priority { 0 } # 低
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -44,6 +44,7 @@ RSpec.describe 'タスク管理機能', type: :model do
         description: 'タスク1です',
         expired_at: Time.now + 60 * 60 * 24
       )
+
       task = Task.new(
         name: 'タスク1',
         description: 'タスク名称の重複は認められません',
@@ -64,58 +65,35 @@ RSpec.describe 'タスク管理機能', type: :model do
 
   describe 'タスク検索機能' do
     before do
-      @task = Task.create(
-        name: 'ポラス新松戸10期',
-        description: '検索テスト用のタスクデータです',
-        expired_at: Time.now + 60 * 60 * 24,
-        status: 'in_progress'
-      )
-
-      @other_task = Task.create(
-        name: 'ポラス流山7期',
-        description: '検索テスト用のタスクデータ2です',
-        expired_at: Time.now + 60 * 60 * 24,
-        status: 'not_started'
-      )
+      @task2 = create :task2
+      @task3 = create :task3
     end
 
-    context 'タスク名を「新松戸」、ステータスは指定なしで検索した場合' do
-      it 'タスク名が「新松戸」を含むタスクを返す' do
-        params = { name: '新松戸', status: '' }
-        expect(Task.search(params)).to include(@task)
+    context 'タスク名を「植栽」、ステータスは「指定なし」で検索した場合' do
+      it 'タスク名が「植栽」を含むタスクを返す' do
+        expect(Task.search(name: '植栽', status: '')).to include(@task2)
       end
 
       it '他のタスクを返さない' do
-        params = { name: '新松戸', status: '' }
-        expect(Task.search(params)).to_not include(@other_task)
+        expect(Task.search(name: '植栽', status: '')).to_not include(@task3)
       end
     end
 
-    context 'タスク名を「テスト」、ステータスは指定なしで検索した場合' do
+    context 'タスク名を「テスト」、ステータスは「指定なし」で検索した場合' do
       it '空を返す' do
-        params = { name: 'テスト', status: '' }
-        expect(Task.search(params)).to be_empty
-      end
-    end
-
-    context 'タスク名を「新松戸」、ステータスは指定なしで検索した場合' do
-      it 'タスク名が「新松戸」を含むタスクを返す' do
-        params = { name: '新松戸', status: '' }
-        expect(Task.search(params)).to include(@task)
+        expect(Task.search(name: 'テスト', status: '')).to be_empty
       end
     end
 
     context 'タスク名は指定なし、ステータスを「着手中」で検索した場合' do
       it 'ステータスが「着手中」のタスクを返す' do
-        params = { name: '', status: 'in_progress' }
-        expect(Task.search(params)).to include(@task)
+        expect(Task.search(name: '', status: 'in_progress')).to include(@task2)
       end
     end
 
-    context 'タスク名を「タスク」、ステータスを「着手中」で検索した場合' do
-      it 'タスク名が「タスク」' do
-        params = { name: '新松戸', status: 'in_progress' }
-        expect(Task.search(params)).to include(@task)
+    context 'タスク名を「伐採」、ステータスを「未着手」で検索した場合' do
+      it 'タスク名が「伐採」を含み、ステータスが「未着手」のタスクを返す' do
+        expect(Task.search(name: '伐採', status: 'not_started')).to include(@task3)
       end
     end
   end
