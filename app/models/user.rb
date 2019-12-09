af09a353186e38b6d83e367dab20fb023278ac17
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-12-10 01:08:30 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 30 }
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
-                    uniqueness: true
+  before_validation { email.downcase! }
 
-  has_many :tasks
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true,
+                    length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, allow_blank: true },
+                    uniqueness: true
+  validates :password,  presence: true,
+                        length: { minimum: 8, allow_blank: true }
+
+  has_secure_password
 end
