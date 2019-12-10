f58c6ee27e52fa206cd242157f23f0a849940705
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-12-10 19:30:36 +0900

    ユーザーのテストデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,17 @@
+10.times do
+  name = Faker::Name.unique.name
+  email = Faker::Internet.unique.email
+  password = 'password!'
+  password_confirmation = 'password!'
+
+  User.create!(
+    name: name,
+    email: email,
+    password: password,
+    password_confirmation: password_confirmation
+  )
+end
+
 (1..100).each do |i|
   Task.create!(
     name: "タスク#{i}",
@@ -6,6 +20,14 @@
     updated_at: Time.now + 60 * 60 * 24 * i,
     expired_at: Time.now + 60 * 60 * 24 * (i + 7),
     status: rand(3),
-    priority: rand(3)
+    priority: rand(3),
+    user_id: rand(10) + 1
   )
 end
+
+# User.create!(
+#   name: '山田太郎',
+#   email: 'yamada@example.com',
+#   password: 'password!',
+#   password_confirmation: 'password!'
+# )
