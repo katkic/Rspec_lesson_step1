1119fe38400b891ef6d37f31fe421f5405fc378d
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-12-10 23:34:55 +0900

    アプリの機能のログイン制限を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+  # herokuのBasic認証
   http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
 
+  before_action :login_required
+
   include SessionsHelper
+
+  private
+
+  def login_required
+    redirect_to root_path, notice: 'ログインしてください' unless logged_in?
+  end
 end
