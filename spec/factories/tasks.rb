98753bc8e4fff5893f666cc1aa62b8e431608066
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-12-15 17:25:12 +0900

    タスクのsystem specを修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :task1, class: Task do
     name { '植栽1' }
     description { 'Factoryで作ったタスク1です' }
-    created_at { Time.new(2019, 12, 5, 10, 0) }
-    expired_at { Time.new(2019, 12, 14, 17, 0) }
+    created_at { Time.current.advance(days: -3) }
+    expired_at { Time.current.advance(days: 5) }
     status { 2 } # 完了
     priority { 2 } # 高
     user
@@ -12,8 +12,8 @@ FactoryBot.define do
   factory :task2, class: Task do
     name { '植栽2' }
     description { 'Factoryで作ったタスク2です' }
-    created_at { Time.new(2019, 12, 6, 10, 0) }
-    expired_at { Time.new(2019, 12, 24, 22, 30) }
+    created_at { Time.current.advance(days: -2) }
+    expired_at { Time.current.advance(days: 10) }
     status { 1 } # 着手中
     priority { 1 } # 中
     user
@@ -22,8 +22,8 @@ FactoryBot.define do
   factory :task3, class: Task do
     name { '伐採1' }
     description { 'Factoryで作ったタスク3です' }
-    created_at { Time.new(2019, 12, 7, 10, 0) }
-    expired_at { Time.new(2019, 12, 31, 17, 0) }
+    created_at { Time.current.advance(days: -1) }
+    expired_at { Time.current.advance(days: 15) }
     status { 0 } # 未着手
     priority { 0 } # 低
     user
