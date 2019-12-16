569ed296ba3b7ba1d01458d22d10585ff766ad95
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-12-16 20:28:08 +0900

    labelsテーブルとlabellingsテーブルを作成

---
+++ db/migrate/20191216112529_create_labellings.rb
@@ -0,0 +1,10 @@
+class CreateLabellings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :labellings do |t|
+      t.integer :task_id, null: false
+      t.integer :label_id, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_15_111836) do
+ActiveRecord::Schema.define(version: 2019_12_16_112529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "labellings", force: :cascade do |t|
+    t.integer "task_id", null: false
+    t.integer "label_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "labels", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", limit: 30, null: false
