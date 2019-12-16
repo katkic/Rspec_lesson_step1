10db50fe53abfda69e902b9d54a8beed90afa0fa
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-12-16 22:05:59 +0900

    タスクとラベルのアソシエーションを設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,8 @@ class Task < ApplicationRecord
   validate :check_expired_at
 
   belongs_to :user
+  has_many :labellings, dependent: :destroy
+  has_many :labels, through: :labellings
   paginates_per 10
 
   enum status: {
